224f8dda5ffca756e799722ac92da5071c1b0526
banyh <banyhong@gmail.com>
2016-08-18 11:20:26 +0800

    add Parsey Universal pretrained models

--- useful_models.sh
+++ useful_models.sh
@@ -56,3 +56,64 @@ rm enwiki_fasttext.tgz
 wget https://gliacloud.blob.core.windows.net/nlp/jawiki_fasttext.tgz
 tar xzf jawiki_fasttext.tgz
 rm jawiki_fasttext.tgz
+cd ..
+
+#
+# Parsey's Cousins: A collection of pretrained syntactic models
+#
+mkdir parsey_universal
+cd parsey_universal
+wget http://download.tensorflow.org/models/parsey_universal/Ancient_Greek-PROIEL.zip
+wget http://download.tensorflow.org/models/parsey_universal/Ancient_Greek.zip
+wget http://download.tensorflow.org/models/parsey_universal/Arabic.zip
+wget http://download.tensorflow.org/models/parsey_universal/Basque.zip
+wget http://download.tensorflow.org/models/parsey_universal/Bulgarian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Catalan.zip
+wget http://download.tensorflow.org/models/parsey_universal/Chinese.zip
+wget http://download.tensorflow.org/models/parsey_universal/Croatian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Czech-CAC.zip
+wget http://download.tensorflow.org/models/parsey_universal/Czech-CLTT.zip
+wget http://download.tensorflow.org/models/parsey_universal/Czech.zip
+wget http://download.tensorflow.org/models/parsey_universal/Danish.zip
+wget http://download.tensorflow.org/models/parsey_universal/Dutch-LassySmall.zip
+wget http://download.tensorflow.org/models/parsey_universal/Dutch.zip
+wget http://download.tensorflow.org/models/parsey_universal/English-LinES.zip
+wget http://download.tensorflow.org/models/parsey_universal/English.zip
+wget http://download.tensorflow.org/models/parsey_universal/Estonian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Finnish-FTB.zip
+wget http://download.tensorflow.org/models/parsey_universal/Finnish.zip
+wget http://download.tensorflow.org/models/parsey_universal/French.zip
+wget http://download.tensorflow.org/models/parsey_universal/Galician.zip
+wget http://download.tensorflow.org/models/parsey_universal/German.zip
+wget http://download.tensorflow.org/models/parsey_universal/Gothic.zip
+wget http://download.tensorflow.org/models/parsey_universal/Greek.zip
+wget http://download.tensorflow.org/models/parsey_universal/Hebrew.zip
+wget http://download.tensorflow.org/models/parsey_universal/Hindi.zip
+wget http://download.tensorflow.org/models/parsey_universal/Hungarian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Indonesian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Irish.zip
+wget http://download.tensorflow.org/models/parsey_universal/Italian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Kazakh.zip
+wget http://download.tensorflow.org/models/parsey_universal/Latin-ITTB.zip
+wget http://download.tensorflow.org/models/parsey_universal/Latin-PROIEL.zip
+wget http://download.tensorflow.org/models/parsey_universal/Latin.zip
+wget http://download.tensorflow.org/models/parsey_universal/Latvian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Norwegian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Old_Church_Slavonic.zip
+wget http://download.tensorflow.org/models/parsey_universal/Persian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Polish.zip
+wget http://download.tensorflow.org/models/parsey_universal/Portuguese-BR.zip
+wget http://download.tensorflow.org/models/parsey_universal/Portuguese.zip
+wget http://download.tensorflow.org/models/parsey_universal/Romanian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Russian-SynTagRus.zip
+wget http://download.tensorflow.org/models/parsey_universal/Russian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Slovenian-SST.zip
+wget http://download.tensorflow.org/models/parsey_universal/Slovenian.zip
+wget http://download.tensorflow.org/models/parsey_universal/Spanish-AnCora.zip
+wget http://download.tensorflow.org/models/parsey_universal/Spanish.zip
+wget http://download.tensorflow.org/models/parsey_universal/Swedish-LinES.zip
+wget http://download.tensorflow.org/models/parsey_universal/Swedish.zip
+wget http://download.tensorflow.org/models/parsey_universal/Tamil.zip
+wget http://download.tensorflow.org/models/parsey_universal/Turkish.zip
+for f in *.zip; unzip $f; end for  # this is FISH syntax
+rm *.zip
