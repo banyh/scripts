c96abe4926f552b5c07d72bae46133509612c9a6
banyh <banyhong@gmail.com>
2016-10-18 23:08:55 +0800

    update

--- install_caffe.sh
+++ install_caffe.sh
@@ -32,32 +32,4 @@ make install
 cp -r install/python/caffe /usr/local/lib/python2.7.12/lib/python2.7/site-packages/
 cp install/lib/*.* /usr/local/lib/
 # make -j16 runtest  # optional
-cd ..
-
-#
-# Install Caffe for Python3 (failed)
-#
-mkdir build2
-cd build2
-cmake -DOpenCV_DIR=/usr/local/lib/python3.5.2/share/OpenCV \
-      -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-7.5 \
-      -DUSE_OPENCV=1 \
-      -DPYTHON_EXECUTABLE=/usr/local/lib/python3.5.2/bin/python3 \
-      -DPYTHON_INCLUDE_DIR=/usr/local/lib/python3.5.2/include/python3.5m \
-      -DPYTHON_LIBRARY=/usr/local/lib/python3.5.2/lib/libpython3.5m.so \
-      ..
-make -j24 all
-cp -r install/python/caffe /usr/local/lib/python3.5.2/lib/python3.5/site-packages/
-
-## Note: build for virtualenv
-# set PROJECT_DIR /home/banyhong/imgsent_demo
-# cmake -DUSE_OPENCV=0 \
-#       -DPYTHON_EXECUTABLE=$PROJECT_DIR/venv/bin/python \
-#       -DPYTHON_INCLUDE_DIR=$PROJECT_DIR/venv/include/python2.7 \
-#       -DPYTHON_LIBRARY=/usr/local/lib/python2.7.12/lib/libpython2.7.so \
-#       /opt/caffe
-# sudo make -j24 all
-# make install
-#
-# pip install protobuf
-# cp -r install/python/caffe $PROJECT_DIR/venv/lib/python2.7/site-packages/
+cd ../..
--- new_machine_2.sh
+++ new_machine_2.sh
@@ -2,63 +2,13 @@
 
 sh install_gpu_2.sh
 
+sh install_python.sh
+
 # install cmake 3.x
 add-apt-repository -y ppa:george-edison55/cmake-3.x
 apt-get update
 apt-get -y install cmake
 apt-get -y upgrade
-
-# install python 2.7.12
-wget https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz
-tar xfz Python-2.7.12.tgz
-rm -f Python-2.7.12.tgz
-cd Python-2.7.12/
-./configure --prefix /usr/local/lib/python2.7.12 --enable-ipv6 --enable-shared
-make -j20
-make install
-echo "/usr/local/lib/python2.7.12/lib" > /etc/ld.so.conf.d/python2.conf
-ldconfig
-cd ..
-ln -sf /usr/local/lib/python2.7.12/bin/python /usr/local/bin/python
-ln -sf /usr/local/lib/python2.7.12/bin/python /usr/local/bin/python2
-hash -d python
-curl https://bootstrap.pypa.io/get-pip.py | python
-ln -sf /usr/local/lib/python2.7.12/bin/pip /usr/local/bin/pip
-
-# install python 3.5.2
-wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
-tar xfz Python-3.5.2.tgz
-rm -f Python-3.5.2.tgz
-cd Python-3.5.2/
-./configure --prefix /usr/local/lib/python3.5.2 --enable-ipv6 --enable-shared
-make -j20
-make install
-echo "/usr/local/lib/python3.5.2/lib" > /etc/ld.so.conf.d/python3.conf
-ldconfig
-cd ..
-ln -sf /usr/local/lib/python3.5.2/bin/python3 /usr/local/bin/python3
-hash -d python3
-curl https://bootstrap.pypa.io/get-pip.py | python3
-ln -sf /usr/local/lib/python3.5.2/bin/pip3 /usr/local/bin/pip3
-
-# install tensorflow for python 2.7
-export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
-
-# install tensorflow for python 3.5
-export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp35-cp35m-linux_x86_64.whl
-pip3 install --upgrade $TF_BINARY_URL
-
-# install Python packages
-pip install scipy nltk gensim janome konlpy jieba pandas jupyter django virtualenv mkdocs
-pip3 install scipy nltk gensim janome konlpy jieba pandas jupyter django virtualenv mkdocs
-
-# install scikit-learn
-pip install scikit-learn scikit-image matplotlib theano keras sk-video
-pip3 install scikit-learn scikit-image matplotlib theano keras sk-video
-
-python -m ipykernel install --user
-python3 -m ipykernel install --user
 
 # install FISH
 apt-add-repository -y ppa:fish-shell/release-2
