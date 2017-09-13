910ed9f290878bc3cb9083876bda21ef13e79a7a
root <root@ip-172-31-27-112.ap-northeast-1.compute.internal>
2017-09-13 03:39:29 +0000

    update

--- install_docker.sh
+++ install_docker.sh
@@ -1,9 +1,9 @@
 apt-get update
-apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu trusty stable"
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu trusty stable"
 
 apt-get update
-apt-get install docker-ce
+apt-get install -y docker-ce
 
