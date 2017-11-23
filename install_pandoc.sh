3a77ef6a6204d6e51f30824d3a0a1c9596020e89
Ping Chu Hung <banyhong@gmail.com>
2017-11-23 10:31:32 +0800

    Create install_pandoc.sh

---
+++ install_pandoc.sh
@@ -0,0 +1,7 @@
+wget https://github.com/jgm/pandoc/releases/download/2.0.3/pandoc-2.0.3-1-amd64.deb
+dpkg -i pandoc-2.0.3-1-amd64.deb
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar xvfz install-tl-unx.tar.gz
+cd install-tl-20171121/
+./install-tl
+export PATH=$PATH:/usr/local/texlive/2017/bin/x86_64-linux
