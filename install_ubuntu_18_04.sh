bcd80cf1466c002fe65dc4155e551f642ba6113e
Ping Chu Hung <banyhong@gliacloud.com>
2019-05-16 01:44:33 +0800

    Update install_ubuntu_18_04.sh

--- install_ubuntu_18_04.sh
+++ install_ubuntu_18_04.sh
@@ -8,17 +8,17 @@
 # reboot
 
 # CUDA 10.0 網路版 (會同時安裝最新的 nvidia driver)
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.168-1_amd64.deb
+dpkg -i cuda-repo-ubuntu1804_10.1.168-1_amd64.deb
 apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 apt-get update
 apt-get install -y cuda
 
 # 安裝CUDNN 7.0 (如果安裝CUDA 9.x，則唯一能配對的版本是CUDNN 7.0)
-wget https://storage.googleapis.com/gliacloud-package/cudnn_ubuntu_18.04/libcudnn7_7.4.2.24-1+cuda10.0_amd64.deb
-wget https://storage.googleapis.com/gliacloud-package/cudnn_ubuntu_18.04/libcudnn7-dev_7.4.2.24-1+cuda10.0_amd64.deb
-dpkg -i libcudnn7_7.4.2.24-1+cuda10.0_amd64.deb
-dpkg -i libcudnn7-dev_7.4.2.24-1+cuda10.0_amd64.deb
+wget https://storage.googleapis.com/gliacloud-package/cudnn_ubuntu_18.04/libcudnn7_7.5.1.10-1+cuda10.1_amd64.deb
+wget https://storage.googleapis.com/gliacloud-package/cudnn_ubuntu_18.04/libcudnn7-dev_7.5.1.10-1+cuda10.1_amd64.deb
+dpkg -i libcudnn7_7.5.1.10-1+cuda10.1_amd64.deb
+dpkg -i libcudnn7-dev_7.5.1.10-1+cuda10.1_amd64.deb
 
 # ----------------------------------------------------------------------------------------------
 # Install dependencies
