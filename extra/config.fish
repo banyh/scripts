44272375aa26ee9414379ad2922d29f34b9e690e
Ping Chu Hung <banyhong@gliacloud.com>
2019-09-17 09:54:05 +0800

    Update config.fish

--- extra/config.fish
+++ extra/config.fish
@@ -1,27 +1,37 @@
 alias ll 'ls -laFh'
 alias l 'ls -CF'
 alias du 'du -h'
-alias df 'df -h'
+alias df 'df -h -x squashfs -T -x tmpfs'
 alias .. 'cd ..'
 alias ... 'cd ../..'
-alias vs 'rsub -p 52689'
-alias sub 'rsub -p 52698'
 alias delh 'echo all | history --delete --prefix locate & echo 1 | history --delete --prefix locate'
+alias op gnome-open
 
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
-set -x PATH ~/bin /usr/local/lib/python2.7.15/bin /usr/local/lib/python3.6.6/bin $PATH /usr/local/texlive/2017/bin/x86_64-linux
-# set -x LOCATE_PATH /var/lib/mlocate/backup.db:/var/lib/mlocate/synology.db
-
-# git config --global user.name "banyh"
-# git config --global user.email banyhong@gliacloud.com
+set -x PATH ~/bin /usr/local/lib/python3.7.1/bin $PATH
+set -x LOCATE_PATH /var/lib/mlocate/backup.db:/var/lib/mlocate/qnap.db
 
 # for better powerline display, disable original virtualenv prompt
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+# for python package "better_exceptions"
+set -x BETTER_EXCEPTIONS 1
 
 function fish_prompt
-  ~/bin/powerline-shell/powerline-shell.py --shell bare --mode patched
+    powerline-shell --shell bare $status
 end
-function fish_right_prompt
-    ~/bin/powerline-shell/powerline-shell-right.py --shell bare --mode patched
+
+function staging
+  set -xg CLUSTER g8s-staging
+  gcloud container clusters get-credentials g8s-staging --zone=asia-east1-a
+  set -xg GIMG2VEC gimg2vec.gliavideo.cn
+  eval (kubectl get svc | grep "ExternalName" | grep -v "admin" | awk '{gsub(/-/, "_"); print "export " toupper($1) "_ROUTE" "=" $1 ".default.githubhero.com"}')
+  eval (kubectl get svc | grep "ExternalName" | grep -v "admin" | awk '{gsub(/-/, "_"); print "export " toupper($1) "=" "35.187.155.48"}')
+end
+function knative
+  set -xg CLUSTER g8s-knative
+  set -xg GIMG2VEC gimg2vec.gliavideo.cn
+  gcloud container clusters get-credentials g8s-knative --zone=asia-east1-a
+  eval (kubectl get svc | grep "ExternalName" | grep -v "admin" | awk '{gsub(/-/, "_"); print "export " toupper($1) "_ROUTE" "=" $1 ".default.gliavideo.cn"}')
+  eval (kubectl get svc | grep "ExternalName" | grep -v "admin" | awk '{gsub(/-/, "_"); print "export " toupper($1) "=" "35.234.20.242"}')
 end
